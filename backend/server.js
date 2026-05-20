251ae935178b89071753a8ef7edda5f8ca71b023
Guilherme <guilhermeabianchini@gmail.com>
2026-05-20 16:41:57 -0300

    Atualização server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.use(cors({
-  origin: "https://gamewired.vercel.app",
+  origin: ["https://gamewired.vercel.app",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"],
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
