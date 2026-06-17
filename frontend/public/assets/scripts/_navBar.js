0f624c02daac5412ef81a3632897396e87b99ab6
Guilherme <guilhermeabianchini@gmail.com>
2026-06-17 20:00:03 -0300

    Alterações no CSS do card de notícias + correção da aba de plataformas no header

--- frontend/public/assets/scripts/_navBar.js
+++ frontend/public/assets/scripts/_navBar.js
@@ -1,12 +1,28 @@
 function openMenu() {
   const navMenu = document.querySelector(".nav__mobile");
   if (navMenu.classList.contains("active")) {
-    navMenu.classList.remove("active");
+    navMenu.classList.remove("active")
   } else {
-    navMenu.classList.add("active");
+    navMenu.classList.add("active")
   }
 }
 
+document.addEventListener("click", (e) => {
+    const btn = e.target.closest(".platforms-btn")
+
+    if (!btn) return
+
+    const platformItems = document.querySelector(".platforms-items")
+    const arrow = document.querySelector(".arrow")
+
+    platformItems.classList.toggle("open")
+
+    arrow.textContent =
+        platformItems.classList.contains("open")
+            ? "▲"
+            : "▼"
+})
+
 document.querySelectorAll(".nav-link").forEach((link) => {
   link.addEventListener("click", () => toggleMenu());
-});
+})
